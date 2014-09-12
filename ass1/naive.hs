d17d100b56e7e3e59289212932e0fc3ebd87b54c
Wobbo <rene.mellema@gmail.com>
2014-09-12 13:19:35 +0200

    Added the cntPrimes function.

--- ass1/naive.hs
+++ ass1/naive.hs
@@ -1,5 +1,10 @@
 isPrime :: Integer -> Bool
+isPrime 1 = False
 isPrime 2 = True
 isPrime n
     | even n = False
     | otherwise = and [ n `mod` x /= 0 | x <- [3,5..(n `div` 2)]]
+
+cntPrimes :: Integer -> Int
+cntPrimes n = length [x | x <- [1..n], isPrime x]
+
