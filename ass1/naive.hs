8ea64de8a43e71f1e8b6d5fb796270db12ed6190
XeryusTC <armadillo@onenetbeyond.org>
2014-09-13 16:00:01 +0200

    Fix expmod to work a bit faster

--- ass1/naive.hs
+++ ass1/naive.hs
@@ -13,15 +13,15 @@ oddPspTO a upb = [ n | n <- [3, 5..upb], a^(n-1) `mod` n == 1, not (isPrime n)]
 
 expmod :: Integer -> Integer -> Integer -> Integer
 expmod a e n
+    | a >  n    = expmod (a `mod` n) e n
+    | e == 1    = a `mod` n
     | e == 0    = 1 `mod` n
     | a == 0    = 0
-    | e == 1    = a `mod` n
-    | a >  n    = expmod (a `mod` n) e n
-    | otherwise = expmod (a ^ divider) (e `div` divider) n
-    where divider = head [x | x <- 2:[3,5..e], e `mod` x == 0]
+    | even e	= (expmod (a * a) (e `div` 2) n)
+    | otherwise = (expmod a (e - 1) n) * a `mod` n
 
 prop_expmod :: Integer -> Integer -> Integer -> Bool
-prop_expmod a e n = ((n <= 0) || (e < 0)) ||
+prop_expmod a e n = ((n <= 0) || (e < 0) || (a < 1)) ||
     (((a ^ e) `mod` n) == (expmod a e n))
 
 oddPspTO' ::Integer -> Integer -> [Integer]
