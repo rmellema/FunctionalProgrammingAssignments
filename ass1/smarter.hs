0d23f6d75a4169b3c5f5a2c2969d872c0a852676
Wobbo <rene.mellema@gmail.com>
2014-09-17 16:41:19 +0200

    Made order work

--- ass1/smarter.hs
+++ ass1/smarter.hs
@@ -1,3 +1,14 @@
+import Data.List
+
+expmod :: Integer -> Integer -> Integer -> Integer
+expmod a e n
+    | a >  n    = expmod (a `mod` n) e n
+    | e == 1    = a `mod` n
+    | e == 0    = 1 `mod` n
+    | a == 0    = 0
+    | even e	= (expmod (a * a) (e `div` 2) n)
+    | otherwise = (expmod a (e - 1) n) * a `mod` n
+
 -- The order from the exercise description
 order' :: Integer -> Integer -> Integer
 order' a p = ord a (a `mod` p) 1 p
@@ -15,6 +26,5 @@ primeFactors n = pF n 2
             | otherwise      = pF a (p+2)
 
 order :: Integer -> Integer -> Integer
-order a p =
+order a p = head [ x | x <- nub (map product (subsequences factors)), expmod a x p == 1]
    where factors = primeFactors (p-1)
-         dfsord a p facs 
