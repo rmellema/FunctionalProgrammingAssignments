74f2a953ae598b6340d0d0d65c0932804b70ddf0
Wobbo <rene.mellema@gmail.com>
2014-09-17 16:07:50 +0200

    Added primeFactors function

--- ass1/smarter.hs
+++ ass1/smarter.hs
@@ -3,5 +3,18 @@ order' :: Integer -> Integer -> Integer
 order' a p = ord a (a `mod` p) 1 p
     where ord a e k p = if e == 1 then k else ord a (a*e `mod` p) (k+1) p
 
+primeFactors :: Integer -> [Integer]
+primeFactors 0 = []
+primeFactors 1 = []
+primeFactors n = pF n 2
+    where pF a p
+            | a == p         = [p]
+            | a `mod` p == 0 = p : pF (a `div` p) p 
+            | a < p          = error "Something went Horribly wrong..."
+            | p == 2         = pF a 3
+            | otherwise      = pF a (p+2)
+
 order :: Integer -> Integer -> Integer
-order a p = 42
+order a p =
+   where factors = primeFactors (p-1)
+         dfsord a p facs 
