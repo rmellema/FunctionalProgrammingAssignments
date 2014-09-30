484570c2ec620a92a446f92c8add3ea74957d67c
Wobbo <rene.mellema@gmail.com>
2014-09-30 11:07:45 +0200

    Added more code for ex5

--- ass2/ex5.hs
+++ ass2/ex5.hs
@@ -1,27 +1,52 @@
-isValidLine :: [Int] -> Bool
+sudoku :: [String]
+sudoku = [ "003020600",
+           "900305001",
+           "001806400",
+           "008102900",
+           "700000008",
+           "006708200",
+           "002609500",
+           "800203009",
+           "005010300" ]
+
+isValidLine :: String -> Bool
 isValidLine [] = True
 isValidLine (x:xs)
-    | x == 0        = isValidLine xs
+    | x == '0'      = isValidLine xs
     | x `elem` xs   = False
     | otherwise     = isValidLine xs
 
-isValidIntSudoku :: [[Int]] -> Bool
-isValidIntSudoku s = and (map isValidLine s)
+isValidColumn :: [String] -> Int -> Bool
+isValidColumn [] _     = True
+isValidColumn (x:xs) n = isValidLine [cs !! n | cs <- (x:xs)]
 
-isFilledLine :: [Int] -> Bool
+isValidRow :: [String] -> Int -> Bool
+isValidRow [] _     = True
+isValidRow (x:xs) n = isValidLine ((x:xs) !! n)
+
+isValidSudoku :: [String] -> Bool
+isValidSudoku s = and (concat (map isValidLine s) (map (isValidColumn s) [1..9]))
+
+isFilledLine :: String -> Bool
 isFilledLine [] = True
 isFilledLine (x:xs)
-    | x == 0    = False
+    | x == '0'  = False
     | otherwise = isFilledLine xs
 
-isFilledIntSudoku :: [[Int]] -> Bool
-isFilledIntSudoku s = and (map isFilledLine s)
+isFilledSudoku :: [String] -> Bool
+isFilledSudoku s = and (map isFilledLine s)
 
-toIntLine :: String -> [Int]
-toIntLine [] = []
-toIntLine (x:xs) = (fromEnum x - offset) : toIntLine xs
-    where offset = fromEnum '0'
+repZeroWith :: String -> Char -> String
+repZeroWith [] _    = []
+repZeroWith (x:xs) n
+    | x == '0'      = n : xs
+    | otherwise     = x : repZeroWith xs n
 
-toIntSudoku :: [String] -> [[Int]]
-toIntSudoku = map toIntLine
+repLines :: String -> [String]
+repLines [] = []
+repLines xs 
+    | '0' `elem` xs  = concat (map repLines (filter isValidLine (map (repZeroWith xs) ['1'..'9'])))
+    | otherwise      = [xs]
 
+allLines :: String -> [String]
+allLines [] = []
