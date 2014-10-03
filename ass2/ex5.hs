f05c7f85254d7d3bacd8d7feb2f347a1cac2b163
XeryusTC <armadillo@onenetbeyond.org>
2014-10-03 13:14:23 +0200

    Meijster fixed the speed of expmod

--- ass2/ex5.hs
+++ ass2/ex5.hs
@@ -1,15 +1,13 @@
 lastDigits :: Int -> Integer -> [Integer]
 lastDigits _ 0 = []
-lastDigits n d = lD number d
-    where number = sum (take n [expmod x x (10^d) | x <- [1..]])
+lastDigits n d = reverse (lD (mod number max) d)
+    where number = sum (take n [expmod x x max | x <- [1..]])
           lD x 0 = []
           lD x d = (x `mod` 10) : lD (x `div` 10) (d-1)
+          max = (10^d)
 
 expmod :: Integer -> Integer -> Integer -> Integer
 expmod a e n
-    | a >  n    = expmod (a `mod` n) e n
-    | e == 1    = a `mod` n
-    | e == 0    = 1 `mod` n
-    | a == 0    = 0
-    | even e    = (expmod (a * a) (e `div` 2) n)
+    | e == 0    = 1
+    | even e    = (expmod ((a * a) `mod` n) (e `div` 2) n)
     | otherwise = (expmod a (e - 1) n) * a `mod` n
