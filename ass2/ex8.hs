32b85297f6c6cf500b2f680d5b5e8a21a63a2f4a
Wobbo <rene.mellema@gmail.com>
2014-10-05 15:29:31 +0200

    Removed everything unneeded for sudoku

--- ass2/ex8.hs
+++ ass2/ex8.hs
@@ -11,24 +11,6 @@ sudoku = [ "003020600",
            "800203009",
            "005010300" ]
 
-isValidLine :: String -> Bool
-isValidLine [] = True
-isValidLine (x:xs)
-    | x == '0'      = isValidLine xs
-    | x `elem` xs   = False
-    | otherwise     = isValidLine xs
-
-isValidColumn :: Sudoku -> Int -> Bool
-isValidColumn [] _     = True
-isValidColumn (x:xs) n = isValidLine [cs !! n | cs <- (x:xs)]
-
-isValidRow :: Sudoku -> Int -> Bool
-isValidRow [] _     = True
-isValidRow (x:xs) n = isValidLine ((x:xs) !! n)
-
-isValidSudoku :: Sudoku -> Bool
-isValidSudoku s = and ((map isValidLine s) ++ (map (isValidColumn s) [1..9]))
-
 isFilledLine :: String -> Bool
 isFilledLine = not . elem '0'
 
@@ -43,13 +25,3 @@ replace v w (x:xs)
 
 replaceZero :: Char -> String -> String
 replaceZero = replace '0'
-
-repLines :: String -> [String]
-repLines [] = []
-repLines xs 
-    | '0' `elem` xs  = concat $ map repLines
-                            $ filter isValidLine [replaceZero x xs | x <- ['1'..'9']]
-    | otherwise      = [xs]
-
-allLines :: String -> [String]
-allLines [] = []
