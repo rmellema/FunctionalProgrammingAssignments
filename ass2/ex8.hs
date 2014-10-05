b83f5dc0196aca86172493dad01f6fc4c840d8dc
Wobbo <rene.mellema@gmail.com>
2014-10-05 16:46:57 +0200

    Made the sudoku exercise

--- ass2/ex8.hs
+++ ass2/ex8.hs
@@ -17,11 +17,40 @@ isFilledLine = not . elem '0'
 isFilledSudoku :: Sudoku -> Bool
 isFilledSudoku = and . map isFilledLine
 
-replace :: (Eq a) => a -> a -> [a] -> [a]
-replace _ _ []     = []
-replace v w (x:xs)
-    | v == x    = w : xs
-    | otherwise = x : replace v w xs
+replaceAt :: [a] -> Int -> a -> [a]
+replaceAt xs i v = take i xs ++ (v: drop (i+1) xs)
 
-replaceZero :: Char -> String -> String
-replaceZero = replace '0'
+replaceAt2D :: [[a]] -> Int -> Int -> a -> [[a]]
+replaceAt2D xss r c v = take r xss ++ (replaceAt (xss !! r) c v : drop (r+1) xss)
+
+validNumberInRow :: String -> Char -> Bool
+validNumberInRow r n = not $ elem n r
+
+validNumberInSudoku :: Sudoku -> Char -> Int -> Int -> Bool
+validNumberInSudoku sud num row col = 
+    and [validNumberInRow (sud !! row) num, 
+        validNumberInRow [s !! col | s <- sud] num,
+        validNumberInRow (concat [take 3 (drop bCol r) | r <- [sud !! x | x <- [bRow..eRow]]]) num]
+    where bRow = row - (row `mod` 3)
+          eRow = bRow + 2
+          bCol = col - (col `mod` 3)
+
+solve :: Sudoku -> Int -> Int -> [Sudoku]
+solve [] _ _  = []
+solve sud 9 _ = [sud]
+solve sud r 9 = solve sud (r+1) 0
+solve sud r c
+    | (sud !! r) !! c /= '0' = solve sud r (c+1)
+    | otherwise              = concat $ map (\x -> solve x r (c+1)) sols
+      where sols = map (replaceAt2D sud r c) (filter (\x -> validNumberInSudoku sud x r c) ['1'..'9'])
+    
+
+solutionsSudoku :: Sudoku -> [Sudoku]
+solutionsSudoku []      = []
+solutionsSudoku sudoku  = solve sudoku 0 0
+
+isCorrectSudoku :: Sudoku -> Bool
+isCorrectSudoku sud = isCorrect (solutionsSudoku sud)
+    where isCorrect []      = False
+          isCorrect [x]     = True
+          isCorrect (x:xs)  = False
