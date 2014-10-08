cb463f567cc60803f795804c51fd068188c71d67
Wobbo <rene.mellema@gmail.com>
2014-10-08 15:27:44 +0200

    Finished ex4

--- ass3/Expression.hs
+++ ass3/Expression.hs
@@ -49,15 +49,23 @@ vars (Val _) = []
 vars (Var n) = [n]
 vars expr    = vars (leftHand expr) `merge` vars (rightHand expr)
 
+isComplete :: [Name] -> Valuation -> Bool
+isComplete [] _  = True
+isComplete xs [] = False
+isComplete xs ys = and (map (/=Nothing) [lookup x ys | x <- xs])
+
 evalExpr :: Expr -> Valuation -> Integer
-evalExpr (Val n) _      = n
-evalExpr (Var n) v      = sure (lookup n v)
+evalExpr expr val
+    | isComplete (vars expr) val = intEvalExpr expr val
+    | otherwise                  = error "Incomplete evaluation given"
+
+intEvalExpr :: Expr -> Valuation -> Integer
+intEvalExpr (Val n) _      = n
+intEvalExpr (Var n) v      = sure (lookup n v)
     where sure (Just b) = b
-evalExpr (e1 :+: e2) v  = evalExpr e1 v + evalExpr e2 v
-evalExpr (e1 :-: e2) v  = evalExpr e1 v - evalExpr e2 v
-evalExpr (e1 :*: e2) v  = evalExpr e1 v * evalExpr e2 v
-evalExpr (e1 :/: e2) v  = evalExpr e1 v `div` evalExpr e2 v
-evalExpr (e1 :%: e2) v  = evalExpr e1 v `mod` evalExpr e2 v
-
-
+intEvalExpr (e1 :+: e2) v  = intEvalExpr e1 v + intEvalExpr e2 v
+intEvalExpr (e1 :-: e2) v  = intEvalExpr e1 v - intEvalExpr e2 v
+intEvalExpr (e1 :*: e2) v  = intEvalExpr e1 v * intEvalExpr e2 v
+intEvalExpr (e1 :/: e2) v  = intEvalExpr e1 v `div` intEvalExpr e2 v
+intEvalExpr (e1 :%: e2) v  = intEvalExpr e1 v `mod` intEvalExpr e2 v
 
