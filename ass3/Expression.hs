ba2ff96ecf146f8ad8a0a6463ce017efd5c8deb1
Wobbo <rene.mellema@gmail.com>
2014-10-08 14:43:15 +0200

    Made Exercise 1+2

--- ass3/Expression.hs
+++ ass3/Expression.hs
@@ -1,4 +1,5 @@
 module Expression where
+import Data.List
 
 type Name = String
 
@@ -11,4 +12,40 @@ data Expr =
    | Expr :/: Expr
    | Expr :%: Expr
 
+instance Show Expr where
+    show (Val n) = show n
+    show (Var n) = n
+    show (e1 :+: e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
+    show (e1 :-: e2) = "(" ++ show e1 ++ " - " ++ show e2 ++ ")"
+    show (e1 :*: e2) = "(" ++ show e1 ++ " * " ++ show e2 ++ ")"
+    show (e1 :/: e2) = "(" ++ show e1 ++ " / " ++ show e2 ++ ")"
+    show (e1 :%: e2) = "(" ++ show e1 ++ " % " ++ show e2 ++ ")"
 
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+    | x <  y    = x : merge xs (y:ys)
+    | x == y    = x : merge xs ys
+    | otherwise = y : merge (x:xs) ys 
+
+leftHand :: Expr -> Expr
+leftHand (l :+: _) = l
+leftHand (l :-: _) = l
+leftHand (l :*: _) = l
+leftHand (l :/: _) = l
+leftHand (l :%: _) = l
+leftHand otherwise = error "No left hand side!"
+
+rightHand :: Expr -> Expr
+rightHand (_ :+: r) = r
+rightHand (_ :-: r) = r
+rightHand (_ :*: r) = r
+rightHand (_ :/: r) = r
+rightHand (_ :%: r) = r
+rightHand otherwise = error "No right hand side!"
+
+vars :: Expr -> [Name]
+vars (Val _) = []
+vars (Var n) = [n]
+vars expr    = vars (leftHand expr) `merge` vars (rightHand expr)
