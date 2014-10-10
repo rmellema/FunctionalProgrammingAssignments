abbbc6a22057b01bb90084dbf8fb1fc4a570546f
Wobbo <rene.mellema@gmail.com>
2014-10-10 13:37:20 +0200

    Removed leftHand and rightHand

--- ass3/Expression.hs
+++ ass3/Expression.hs
@@ -29,26 +29,14 @@ merge (x:xs) (y:ys)
     | x == y    = x : merge xs ys
     | otherwise = y : merge (x:xs) ys
 
-leftHand :: Expr -> Expr
-leftHand (l :+: _) = l
-leftHand (l :-: _) = l
-leftHand (l :*: _) = l
-leftHand (l :/: _) = l
-leftHand (l :%: _) = l
-leftHand otherwise = error "No left hand side!"
-
-rightHand :: Expr -> Expr
-rightHand (_ :+: r) = r
-rightHand (_ :-: r) = r
-rightHand (_ :*: r) = r
-rightHand (_ :/: r) = r
-rightHand (_ :%: r) = r
-rightHand otherwise = error "No right hand side!"
-
 vars :: Expr -> [Name]
 vars (Val _) = []
 vars (Var n) = [n]
-vars expr    = vars (leftHand expr) `merge` vars (rightHand expr)
+vars (l :+: r) = vars l `merge` vars r
+vars (l :-: r) = vars l `merge` vars r
+vars (l :*: r) = vars l `merge` vars r
+vars (l :/: r) = vars l `merge` vars r
+vars (l :%: r) = vars l `merge` vars r
 
 isComplete :: [Name] -> Valuation -> Bool
 isComplete [] _  = True
