f4693fdc04246d7d79a237461e1af7f7b0d816b3
XeryusTC <armadillo@onenetbeyond.org>
2014-10-15 16:42:26 +0200

    Made the CSP solvah

--- ass3/Main.hs
+++ ass3/Main.hs
@@ -1,5 +1,8 @@
 import Data.Char
 import System.IO
+import Compare
+import Types
+import Valuation
 
 removeWhiteSpace :: String -> String
 removeWhiteSpace s = filter (not.isSpace) s
@@ -8,7 +11,34 @@ readInput :: IO String
 readInput = do input <- hGetContents stdin
                return (removeWhiteSpace input)
 
+solveCSP :: String -> [Valuation]
+solveCSP str = [d | d <- domains, and $ map (\x -> evalCmp x d) constraints]
+    where (domain, constr) = splitCSP str
+          domains          = valuations (map parseDomain (split ',' domain))
+          constraints      = map toComparison (split ',' constr)
+
+splitCSP :: String -> (String, String)
+splitCSP str = (domain, constr)
+    where domain = takeWhile (/= '}') (drop ldomain str)
+          constr = (tail.init) (drop (length domain + ldomain + lconstr) str)
+          ldomain = length "domains{"
+          lconstr = length "constraints{"
+
+split :: Char -> String -> [String]
+split _ [] = []
+split c (str) = takeWhile (/= c) str : split c rem
+    where end = (dropWhile (/= c) str)
+          rem = if null end then [] else tail end
+
+parseDomain :: String -> (Name, Domain)
+parseDomain str = (name, domain)
+    where name   = takeWhile (isAlpha) str
+          first  = read (takeWhile (isDigit) ((drop (length name + lbs)) str)) :: Integer
+          second = read ((init.tail.tail) (dropWhile (/= '.') str)) :: Integer
+          lbs    = length "<-["
+          domain = [first .. second]
+
 main :: IO()
 main = do input <- readInput
-          putStrLn input
+          putStrLn (show (solveCSP input))
           return ()
