9f6ee8c9920a50bfb4d3a7825b24f6d08320e891
XeryusTC <armadillo@onenetbeyond.org>
2014-10-15 15:25:41 +0200

    Implement exercise 1 of week 2

--- ass3/Compare.hs
+++ ass3/Compare.hs
@@ -3,8 +3,10 @@ module Compare where
 import Expression
 
 data Relop = LessThan | LessEqual | Equal | GreaterEqual | Greater | NotEqual
+    deriving Show
 
 data Comparison = Cmp Relop Expr Expr
+    deriving Show
 
 cmpToOp :: (Ord a) => Relop -> (a -> a -> Bool)
 cmpToOp LessThan     = (<)
@@ -17,3 +19,32 @@ cmpToOp NotEqual     = (/=)
 evalCmp :: Comparison -> Valuation -> Bool
 evalCmp (Cmp op e1 e2) val = oper (evalExpr e1 val) (evalExpr e2 val)
     where oper = cmpToOp op
+
+toComparison :: String -> Comparison
+toComparison str = Cmp oper (toExpr left) (toExpr right)
+    where left  = leftHand str
+          op    = findOp (drop (length left) str)
+          right = drop ((length left) + (length op)) str
+          oper
+            | op == "<"  = LessThan
+            | op == "<=" = LessEqual
+            | op == "="  = Equal
+            | op == ">=" = GreaterEqual
+            | op == ">"  = Greater
+            | op == "#"  = NotEqual
+            | otherwise = error "Incorrect comparison operator"
+
+leftHand :: String -> String
+leftHand [] = []
+leftHand (s:str)
+    | isComp s  = []
+    | otherwise = s : (leftHand str)
+
+findOp :: String -> String
+findOp [] = []
+findOp (s:str)
+    | isComp s  = s : (findOp str)
+    | otherwise = []
+
+isComp :: Char -> Bool
+isComp c = c == '<' || c == '=' || c == '>' || c == '#'
