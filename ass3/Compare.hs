f6ecd75ce5a1fc936b0a48e631caf70981d04bec
Wobbo <rene.mellema@gmail.com>
2014-10-15 15:10:44 +0200

    Added evalCmp

--- ass3/Compare.hs
+++ ass3/Compare.hs
@@ -6,3 +6,14 @@ data Relop = LessThan | LessEqual | Equal | GreaterEqual | Greater | NotEqual
 
 data Comparison = Cmp Relop Expr Expr
 
+cmpToOp :: Comparison -> (a -> Bool)
+cmpToOp (Cmp LessThan     _ _ ) = (<)
+cmpToOp (Cmp LessEqual    _ _ ) = (<=)
+cmpToOp (Cmp Equal        _ _ ) = (==)
+cmpToOp (Cmp GreaterEqual _ _ ) = (>=)
+cmpToOp (Cmp Greater      _ _ ) = (>)
+cmpToOp (Cmp NotEqual     _ _ ) = (/=)
+
+evalCmp :: Comparison -> Valuation -> Bool
+evalCmp (Cmp op e1 e2) val = oper (evalExpr e1 val) (evalExpr e2 val)
+    where oper = cmpToOp op
